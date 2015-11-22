fd6707e073006d8ecece10278984b439cc77dac2
khalil.chourou <chouroukhalil@gmail.com>
2015-11-23 00:51:54 +0100

    feature selection

---
+++ monthname2number.sh
@@ -0,0 +1,14 @@
+
+
+sed -i 's/Jan/1/g' store.csv
+sed -i 's/Feb/2/g' store.csv
+sed -i 's/Mar/3/g' store.csv
+sed -i 's/Apr/4/g' store.csv
+sed -i 's/May/5/g' store.csv
+sed -i 's/Jun/6/g' store.csv
+sed -i 's/Jul/7/g' store.csv
+sed -i 's/Aug/8/g' store.csv
+sed -i 's/Sept/9/g' store.csv
+sed -i 's/Oct/10/g' store.csv
+sed -i 's/Nov/11/g' store.csv
+sed -i 's/Dec/12/g' store.csv
